02bb7314d3ee2d5419ffa923d9f0a5b9b0bbd1b4
jjradler <joseph.james.radler@gmail.com>
2019-12-14 15:47:14 -0800

    pulled all the submodules again and rewrote pathogen.vim in an effort to figure out why I can't get syntax markup in python on Raspbian.

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/bin/env python3
+"""
+Still it isn't updating the damned syntax...
+"""
+import numpy as np
+
+# This hasn't worked yet... wtf...
+
+if __name__ == "__main__":
+  print("This shit aint working...")
